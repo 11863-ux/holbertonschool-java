3d6cae91e4bdefa9acb12d835f41d5d5362ff907
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-08-07 23:25:50 +0400

    Task 4

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,6 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String text = "Holberton School";
-        System.out.printf(text + "\n" + text + "\n" + text.substring(0, 9) + "\n");
+        System.out.println(text + "\n" + text + "\n" + text.substring(0, 9));
     }
 }
