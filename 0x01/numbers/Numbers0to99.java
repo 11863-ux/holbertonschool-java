2e5868ff0939ffec6f6d086976e3c1fc1ac60415
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-08-08 00:05:32 +0400

    Task 1

---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,13 @@
+package numbers;
+
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        for (int i=0;i<100;i++){
+                System.out.printf(String.valueOf(i));
+                if(i!=99){
+                    System.out.print(", ");
+                }
+        }
+        System.out.print(" ");
+    }
+}
