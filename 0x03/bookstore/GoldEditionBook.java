c8d1edaef49e7b878e964b540966cb6f7c8eb99a
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-08-13 00:02:37 +0400

    Task 2

---
+++ 0x03/bookstore/GoldEditionBook.java
@@ -0,0 +1,14 @@
+package bookstore;
+
+import bookstore.exceptions.InvalidBookException;
+
+public class GoldEditionBook extends Book{
+    GoldEditionBook(String title, String author, double price) {
+        super(title, author, price);
+    }
+
+    @Override
+    public double getPrice() throws InvalidBookException {
+        return super.getPrice()+super.getPrice()*30/100;
+    }
+}
