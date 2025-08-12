11f6e8443cbf28734eb668b136cd9674338974f0
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-08-13 00:02:47 +0400

    Task 2

---
+++ 0x03/bookstore/Book.java
@@ -0,0 +1,37 @@
+package bookstore;
+
+import bookstore.exceptions.InvalidAuthorException;
+import bookstore.exceptions.InvalidBookException;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    public String getTitle() throws InvalidBookException {
+        if(title.length()<3){
+            throw new InvalidBookException("Invalid book title");
+        }
+        return title;
+    }
+
+    public String getAuthor() throws InvalidAuthorException{
+        String[] words = author.split(" ");
+        if(words.length <=1) {
+            throw new InvalidAuthorException("Invalid author name");
+        }
+        return author;
+    }
+
+    public double getPrice() throws InvalidBookException {
+        if(price <= 0){
+            throw new InvalidBookException("Invalid book price");
+        }
+        return price;
+    }
+}
