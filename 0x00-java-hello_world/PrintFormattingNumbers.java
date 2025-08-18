6583ebc3f756bf03730ca3165582e9ac102debfa
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-08-19 01:47:49 +0400

    Task 6

--- 0x00-java-hello_world/PrintFormattingNumbers.java
+++ 0x00-java-hello_world/PrintFormattingNumbers.java
@@ -1,9 +1,8 @@
 public class PrintFormattingNumbers {
     public static void main(String[] args) {
-        float tax = 0.002456f; // Adjusted from 0.2456f to match desired output
+        float tax = 0.2456f;
         float value = 7654.321f;
-
         System.out.printf("Value: $%,.2f\n", value);
-        System.out.printf("Tax: %.2f%%\n", tax * 100);
+        System.out.printf("Tax: %.2f%%\n", tax);
     }
 }
