ba2adbb2fa7d365cb3ae7277f3b0ca4121ed95e0
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-08-19 00:59:57 +0400

    Task 3

--- 0x03/basic_account/BasicBankAccount.java
+++ 0x03/basic_account/BasicBankAccount.java
@@ -1,16 +1,15 @@
 package basic_account;
 
-import basic_account.exceptions.InvalidOperationException;
+import exceptions.InvalidOperationException;
 
 public class BasicBankAccount {
     private String accountNumber;
-    private double balance=0;
+    private double balance = 0;
     private double annualInterestRate;
 
-    public BasicBankAccount(String accountNumber, double annualInterestRate)
-    {
-        this.accountNumber=accountNumber;
-        this.annualInterestRate=annualInterestRate;
+    public BasicBankAccount(String accountNumber, double annualInterestRate) {
+        this.accountNumber = accountNumber;
+        this.annualInterestRate = annualInterestRate;
     }
 
     public String getAccountNumber() {
@@ -25,36 +24,35 @@ public class BasicBankAccount {
         return annualInterestRate;
     }
 
-    void deposit(double value) throws InvalidOperationException {
-        if(value>0){
-            balance+=value;
+    public void deposit(double value) throws InvalidOperationException {
+        if (value > 0) {
+            balance += value;
         } else throw new InvalidOperationException("Deposit amount must be greater than 0");
     }
 
     void withdraw(double value) throws InvalidOperationException {
-        if(value<0){
+        if (value < 0) {
             throw new InvalidOperationException("Withdrawal amount must be greater than 0");
-        }
-        else if(value>balance){
+        } else if (value > balance) {
             throw new InvalidOperationException("Withdrawal amount must be less than balance");
-        }else balance-=value;
+        } else balance -= value;
     }
 
-    double calculateMonthlyFee(){
-        double percentageFee=balance*10/100;
+    double calculateMonthlyFee() {
+        double percentageFee = balance * 10 / 100;
         return Math.min(percentageFee, 10.00);
     }
 
-    double calculateMonthlyInterest(){
-        if(balance<=0){
+    double calculateMonthlyInterest() {
+        if (balance <= 0) {
             return 0;
-        }else{
-            double monthlyInterest=annualInterestRate/12;
-            return balance*monthlyInterest/100;
+        } else {
+            double monthlyInterest = annualInterestRate / 12;
+            return balance * monthlyInterest / 100;
         }
     }
 
-    void applyMonthlyUpdate(){
-        balance=balance-calculateMonthlyFee()+calculateMonthlyInterest();
+    void applyMonthlyUpdate() {
+        balance = balance - calculateMonthlyFee() + calculateMonthlyInterest();
     }
 }
--- 0x03/basic_account/Program.java
+++ 0x03/basic_account/Program.java
@@ -4,44 +4,44 @@ public class Program {
     public static void main(String[] args) {
         try {
             BasicBankAccount account1 = new BasicBankAccount("AAA", 50.0);
-            System.out.printf("Account %s - balance: %.2f\n", account1.getAccountNumber(), account1.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account1.getAccountNumber(), account1.getBalance()).replace('.', ','));
             account1.deposit(100);
-            System.out.printf("Account %s - balance: %.2f\n", account1.getAccountNumber(), account1.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account1.getAccountNumber(), account1.getBalance()).replace('.', ','));
             account1.withdraw(25);
-            System.out.printf("Account %s - balance: %.2f\n", account1.getAccountNumber(), account1.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account1.getAccountNumber(), account1.getBalance()).replace('.', ','));
             account1.applyMonthlyUpdate();
-            System.out.printf("Account %s - balance: %.2f\n\n", account1.getAccountNumber(), account1.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f\n", account1.getAccountNumber(), account1.getBalance()).replace('.', ','));
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
             BasicBankAccount account2 = new BasicBankAccount("BBB", 100.0);
-            System.out.printf("Account %s - balance: %.2f\n", account2.getAccountNumber(), account2.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account2.getAccountNumber(), account2.getBalance()).replace('.', ','));
             account2.deposit(10);
-            System.out.printf("Account %s - balance: %.2f\n", account2.getAccountNumber(), account2.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account2.getAccountNumber(), account2.getBalance()).replace('.', ','));
             account2.withdraw(10);
-            System.out.printf("Account %s - balance: %.2f\n", account2.getAccountNumber(), account2.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account2.getAccountNumber(), account2.getBalance()).replace('.', ','));
             account2.applyMonthlyUpdate();
-            System.out.printf("Account %s - balance: %.2f\n\n", account2.getAccountNumber(), account2.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f\n", account2.getAccountNumber(), account2.getBalance()).replace('.', ','));
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
             BasicBankAccount account3 = new BasicBankAccount("CCC", 120.0);
-            System.out.printf("Account %s - balance: %.2f\n", account3.getAccountNumber(), account3.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account3.getAccountNumber(), account3.getBalance()).replace('.', ','));
             account3.deposit(1600);
-            System.out.printf("Account %s - balance: %.2f\n", account3.getAccountNumber(), account3.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account3.getAccountNumber(), account3.getBalance()).replace('.', ','));
             account3.applyMonthlyUpdate();
-            System.out.printf("Account %s - balance: %.2f\n\n", account3.getAccountNumber(), account3.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f\n", account3.getAccountNumber(), account3.getBalance()).replace('.', ','));
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
         }
 
         try {
             BasicBankAccount account4 = new BasicBankAccount("DDD", 70.0);
-            System.out.printf("Account %s - balance: %.2f\n", account4.getAccountNumber(), account4.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account4.getAccountNumber(), account4.getBalance()).replace('.', ','));
             account4.deposit(0);
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
@@ -49,9 +49,9 @@ public class Program {
 
         try {
             BasicBankAccount account5 = new BasicBankAccount("EEE", 50.0);
-            System.out.printf("Account %s - balance: %.2f\n", account5.getAccountNumber(), account5.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account5.getAccountNumber(), account5.getBalance()).replace('.', ','));
             account5.deposit(25);
-            System.out.printf("Account %s - balance: %.2f\n", account5.getAccountNumber(), account5.getBalance());
+            System.out.println(String.format("Account %s - balance: %.2f", account5.getAccountNumber(), account5.getBalance()).replace('.', ','));
             account5.withdraw(30);
         } catch (Exception ex) {
             System.out.println(ex.getMessage() + "\n");
