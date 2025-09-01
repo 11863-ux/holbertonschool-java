6ebfb5b13756e645e449dd56f2324d3a791b2b78
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-09-01 12:39:28 +0400

    task 1

--- PrimitiveTypes_Wrapper/positive_integer/PositiveInteger.java
+++ PrimitiveTypes_Wrapper/positive_integer/PositiveInteger.java
@@ -1,3 +1,5 @@
+package positive_integer;
+
 public class PositiveInteger {
     private int value;
 
--- PrimitiveTypes_Wrapper/positive_integer/Program.java
+++ PrimitiveTypes_Wrapper/positive_integer/Program.java
@@ -1,3 +1,5 @@
+package positive_integer;
+
 public class Program {
     public static void main(String[] args) {
         try {
