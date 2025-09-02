591fa082573c901ad5e32d189779e51509650d22
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-09-02 10:07:23 +0400

    task 1

--- List/numbers/Program.java
+++ List/numbers/Program.java
@@ -1,3 +1,8 @@
+package numbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<Integer>();
