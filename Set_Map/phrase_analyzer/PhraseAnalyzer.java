e4b80ab9c7275cf27e211b58a2fb035dbebda19e
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-09-08 01:22:44 +0400

    Task 1

--- Set_Map/phrase_analyzer/PhraseAnalyzer.java
+++ Set_Map/phrase_analyzer/PhraseAnalyzer.java
@@ -4,7 +4,7 @@ import java.util.TreeMap;
 public class PhraseAnalyzer {
     public static TreeMap<String,Integer> wordCount(String phrase){
         TreeMap<String,Integer> map = new TreeMap<>();
-        phrase = phrase.toLowerCase().replaceAll("[?.!]","");
+        phrase = phrase.toLowerCase().replaceAll("[?.!,]","");
         List<String> listOfWords= List.of(phrase.split(" "));
         for(String word : listOfWords){
             int count=0;
