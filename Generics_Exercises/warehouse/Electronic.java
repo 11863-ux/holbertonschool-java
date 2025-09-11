92b168cd64f7e23607354a44c953ce5e5eaf95e5
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-09-11 16:28:27 +0400

    task 1

---
+++ Generics_Exercises/warehouse/Electronic.java
@@ -0,0 +1,14 @@
+public class Electronic extends Item {
+    String description;
+    double value;
+
+    Electronic(String description, double value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] $ %.5f",description,value);
+    }
+}
---
+++ Generics_Exercises/warehouse/Food.java
@@ -0,0 +1,16 @@
+public class Food extends Item {
+    String name;
+    double calories;
+    double price;
+
+    Food(String name, double calories, double price) {
+        this.name = name;
+        this.calories = calories;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %.5f $ %.5f",name,calories,price);
+    }
+}
