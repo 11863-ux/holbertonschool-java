728d8444a01a7c65e88ad423a5cd71a0375ec329
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-09-12 12:56:53 +0400

    task 1

--- Lambda_Exercises/search_products/ProductSearch.java
+++ Lambda_Exercises/search_products/ProductSearch.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductSearch {
-    public static List<Product> filter(List<Product> products, FilterCriteria criteria) {
+    public static List<Product> filter(Product[] products, FilterCriteria criteria) {
         List<Product> filteredProducts = new ArrayList<>();
         for (Product product : products) {
             if (criteria.test(product)) {
