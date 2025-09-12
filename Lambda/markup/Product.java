3420956067e941135fd22ba61e92b33d1c9cfe2b
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-09-12 11:36:43 +0400

    task 1

---
+++ Lambda/markup/Product.java
@@ -0,0 +1,29 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Product {
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getMarkupPercentage() {
+        return markupPercentage;
+    }
+
+    String name;
+    double price;
+    double markupPercentage=10;
+
+    Product(double price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    Supplier<Double> priceWithMarkup= ()-> price+price*markupPercentage/100;
+    Consumer<Double> updateMarkup=(Double value) -> markupPercentage-=value;
+
+}
