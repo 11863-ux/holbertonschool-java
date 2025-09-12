2ab59f3f5b169f898a27f386d0ea8d0cbd465048
Mirhabil <mirhabil.sariyev2005vv@gmail.com>
2025-09-12 12:01:17 +0400

    task 2

---
+++ Lambda/net/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    double salary;
+
+    Employee(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
