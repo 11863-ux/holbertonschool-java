487f4853d2174dac920e21880abc2351b49f78e5
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-09-20 18:32:48 +0400

    Task 4

---
+++ Streams_Collectors_Optional/electronic_orders/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+
+    int code;
+    String name;
+    ProductCategory category;
+    public double price;
+
+    public Product(int code, String name, ProductCategory category, double price) {
+        this.code = code;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s %s $ %.2f", code, name, category, price).replace(".", ",");
+    }
+}
