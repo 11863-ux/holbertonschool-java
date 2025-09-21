462d47559f86c264082a9fe957819029abf60068
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-09-22 02:55:23 +0400

    Task 4

---
+++ Streams_Collector_Optional_Exercices/average_salary_position/PeopleQuery.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PeopleQuery {
+
+    public static TreeMap<String, TreeSet<Person>> getPeopleGroupedByPositionInReverseOrder(List<Person> people){
+        return people.stream().collect(Collectors.groupingBy(Person::getPosition,()->new TreeMap<>(Comparator.reverseOrder()),Collectors.toCollection(()->new TreeSet<>(Comparator.comparing(Person::getCode)))));
+
+    }
+
+
+    public static Map<String, Long> getPeopleCountByPosition(List<Person> people){
+        return people.stream().collect(Collectors.groupingBy(Person::getPosition,Collectors.counting())).entrySet().stream()
+                .sorted(Comparator.comparing((Map.Entry<String, Long> entry)->entry.getKey().length()).reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1,e2)->e1,LinkedHashMap::new));
+    }
+
+    public static  Map<String, Map<Integer, Long>> getPeopleCountByPositionAndAge(List<Person> people){
+        return people.stream().collect(Collectors.groupingBy(Person::getPosition,Collectors.groupingBy(Person::getAge,Collectors.counting())))
+                .entrySet().stream().sorted(Comparator.comparing((Map.Entry<String, Map<Integer, Long>> entry) -> entry.getKey().length()).reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(e1,e2)->e1,LinkedHashMap::new));
+    }
+
+    public static Map<String, Double> getAverageSalaryByPosition(List<Person> people){
+        return people.stream().collect(Collectors.groupingBy(Person::getPosition,Collectors.averagingDouble(Person::getSalary))).entrySet().stream()
+                .sorted(Comparator.comparing((Map.Entry<String, Double> entry)->entry.getKey().length()).reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1,e2)->e1,LinkedHashMap::new));
+    }
+}
