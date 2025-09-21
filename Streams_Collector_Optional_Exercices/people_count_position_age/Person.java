006e5c4cc86db87c9bdafb724dd2a8af6c72dd93
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-09-22 02:43:28 +0400

    Task 3

---
+++ Streams_Collector_Optional_Exercices/people_count_position_age/Person.java
@@ -0,0 +1,41 @@
+public class Person {
+    int code;
+    String name;
+    String position;
+    int age;
+    double salary;
+
+    public Person(int code, String name, String position, int age, double salary) {
+        this.code = code;
+        this.name = name;
+        this.position = position;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s %s %d $ %f",code,name,position,age,salary);
+    }
+
+}
