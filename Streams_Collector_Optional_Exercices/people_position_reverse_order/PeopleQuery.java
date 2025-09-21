ac40b9fce0a252b93b4ad70f8a63ea5811f2243a
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-09-21 13:15:02 +0400

    Task 1

---
+++ Streams_Collector_Optional_Exercices/people_position_reverse_order/PeopleQuery.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class PeopleQuery {
+
+    public static TreeMap<String, TreeSet<Person>> getPeopleGroupedByPositionInReverseOrder(List<Person> people){
+        return people.stream().collect(Collectors.groupingBy(Person::getPosition,()->new TreeMap<>(Comparator.reverseOrder()),Collectors.toCollection(()->new TreeSet<>(Comparator.comparing(Person::getCode)))));
+
+    }
+}
