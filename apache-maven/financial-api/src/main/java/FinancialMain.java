31702883f8c4bb48efbe6560e5bce17a494a61a2
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-09-28 16:58:59 +0400

    Task 1

--- apache-maven/financial-api/src/main/java/FinancialMain.java
+++ apache-maven/financial-api/src/main/java/FinancialMain.java
@@ -1,6 +1,5 @@
 class FinancialMain {
     public static void main(String[] args) {
-        String message = "'Creating a project Java Maven'";
-        System.out.println(message);
+        System.out.println("Creating a project Java Maven");
     }
 }
