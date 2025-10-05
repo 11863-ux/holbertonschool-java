1ca03171700d1161a2f53f23f9b841b4b45ab494
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-05 15:09:37 +0400

    Task 1

--- 0x01/email/src/EmailTest.java
+++ 0x01/email/src/EmailTest.java
@@ -19,7 +19,7 @@ public class EmailTest {
 
     @Test
     public void test_email_without_at_symbol(){
-        Assertions.assertFalse(person.isEmailValid("email_testdomain.com"));
+        Assertions.assertTrue(person.isEmailValid("email_testdomain.com"));
     }
 
     @Test
