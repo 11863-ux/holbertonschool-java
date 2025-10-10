20c3b43d9725880b99d675e9213f262236d28972
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-11 02:50:07 +0400

    Task 1

--- 0x01/spring_boot_1/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -1,4 +1,14 @@
 package example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageResource {
+
+    @RequestMapping(value = "/messages")
+    public String simpleMessageWelcome() {
+        return "WELCOME TO THE MICRO-SERVICE CLASS USING SPRING BOOT!!!";
+    }
+
 }
