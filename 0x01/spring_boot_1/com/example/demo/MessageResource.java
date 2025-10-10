ebf312970a8a4f1cb67f4d830db366646816b678
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-11 03:10:13 +0400

    Task 1

--- 0x01/spring_boot_1/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -1,6 +1,7 @@
 package example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,16 @@ public class MessageResource {
     @GetMapping(value = "/simpleMessageWelcome")
     public String simpleMessageWelcome() {
         return "WELCOME TO THE MICRO-SERVICE CLASS USING SPRING BOOT!!!";
+    }
+
+    @GetMapping(value = "/login/{username}/{password}")
+    public String login(@PathVariable String username, @PathVariable String password) {
+        if(username.isEmpty() || password.isEmpty()) {
+            return "USER AND PASSWORD NOT PROVIDED";
+        }
+        else if(username.length()>15 || password.length()>15) {
+            return "USER AND PASSWORD INVALID";
+        }else return "LOGIN SUCCESSFUL!!!";
     }
 
 }
