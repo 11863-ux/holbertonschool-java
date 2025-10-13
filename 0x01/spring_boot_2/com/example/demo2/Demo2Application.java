941c19e709fd8af2d46bc5168097b6cde81ebac0
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-14 00:41:45 +0400

    Task 2

--- 0x01/spring_boot_2/com/example/demo2/Demo2Application.java
+++ 0x01/spring_boot_2/com/example/demo2/Demo2Application.java
@@ -2,10 +2,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot2Application {
+public class Demo2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot2Application.class, args);
+        SpringApplication.run(Demo2Application.class, args);
     }
 
 }
