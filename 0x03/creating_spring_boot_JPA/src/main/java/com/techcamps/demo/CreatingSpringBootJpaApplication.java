91095df9c0208731376ffee8b5e4107a144dbd11
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-15 09:47:41 +0400

    Task 1

---
+++ 0x03/creating_spring_boot_JPA/src/main/java/com/techcamps/demo/CreatingSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.techcamps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingSpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingSpringBootJpaApplication.class, args);
+    }
+
+}
