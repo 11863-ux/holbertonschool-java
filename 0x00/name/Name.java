dfd2a566dad4f47079d9f9f629cf20a7af8a3c87
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-21 00:14:14 +0400

    Task 1

--- 0x00/name/Name.java
+++ 0x00/name/Name.java
@@ -5,8 +5,9 @@ public class Name {
     public List<Testcase> findAllTestcasesByUser(Name.User user){};
 
     // Correct Declarations
+    // A
+    public List<Account> AccountList;
     // B
-    public Account[] accountList;
 
     // No Prefixes
     // B
