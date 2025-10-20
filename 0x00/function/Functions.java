a3fc592c9b0ddd25a3511cb31a2995c5660b7b2c
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-21 00:18:30 +0400

    Task 2

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,44 @@
+public class Functions {
+    // Do only 1 thing
+    // B
+    public void saveIncome(Income income){
+        inRepo.save(income);
+    }
+    public void saveExpense(Expense expense){
+        outRepo.save(expense);
+    }
+
+    // Only 1 level of abstraction
+    // A
+    @PostMapping("/user")
+    public User saveUser(User user){
+        return userService.save(user);
+    }
+
+    // Arguments
+    // A
+    public void saveUser(User user){};
+
+    // No side effects
+
+    // B
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+    public void setRole(User user){
+        user.setRole(user);
+    }
+
+    // Avoid duplicates by extracting into new methods
+    // B
+    public Environment getEnvironment(){
+        return environmentService.getEnvironment(getVersion());
+    }
+    public Release getRelease(){
+        return releaseService.getRelease(getVersion());
+    }
+    private String getVersion(){
+        return versionService.getVersion();
+    }
+
+}
