0ba630c94c7f84ddcfbdb4f1caf09be2ba625e62
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-22 19:18:38 +0400

    Task 1

---
+++ 0x08/ProductAPI/src/main/java/com/example/product/controller/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.example.product.controller;
+
+public class ApiResponse <T>{
+
+    private T data;
+    private String description;
+    private int responseCode;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+}
--- 0x08/ProductAPI/src/main/java/com/example/product/controller/ProductController.java
+++ 0x08/ProductAPI/src/main/java/com/example/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.product.controller;
 
 import com.example.product.model.Product;
 import com.example.product.model.ProductRepository;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,40 +19,57 @@ public class ProductController {
      * Responsible for returning a list of products.
      */
     @GetMapping(value = "/allProducts")
-    public List<Product> getAllProducts() {
-        return productRepository.getAllProducts();
+    public ApiResponse<List<Product>> getAllProducts() {
+        ApiResponse response=new ApiResponse();
+        response.setData(productRepository.getAllProducts());
+        response.setDescription("Warning - the process returned more than 1000 products.");
+        response.setResponseCode(11);
+        return response;
     }
 
     /**
      * Responsible for returning a product by its ID.
      */
     @GetMapping(value = "/findProductById/{id}")
-    public Product getProductById(@PathVariable Long id) {
-        return productRepository.getProductById(id);
+    public ApiResponse<Product> getProductById(@PathVariable Long id) {
+        ApiResponse response=new ApiResponse();
+        response.setData(productRepository.getProductById(id));
+        response.setDescription("The field id not informed. This information is required.");
+        response.setResponseCode(12);
+        return response;
     }
 
     /**
      * Responsible for adding a product.
      */
     @PostMapping(value = "/addProduct")
-    public void addProduct(@RequestBody Product p) {
-        productRepository.addProduct(p);
+    public ApiResponse addProduct(@RequestBody Product p) {
+        ApiResponse response=new ApiResponse();
+        response.setDescription("Required fields not informed.");
+        response.setResponseCode(10);
+        return response;
     }
 
     /**
      * Responsible for updating a product.
      */
     @PutMapping(value = "/updateProduct")
-    public void updateProduct(@RequestBody Product p) {
-        productRepository.updateProduct(p);
+    public ApiResponse updateProduct(@RequestBody Product p) {
+        ApiResponse response=new ApiResponse();
+        response.setDescription("No information has been updated. The new information is the same as recorded in the database.");
+        response.setResponseCode(14);
+        return response;
     }
 
     /**
      * Responsible for removing a product.
      */
     @DeleteMapping(value = "/removeProduct")
-    public void removeProduct(@RequestBody Product p) {
-        productRepository.removeProduct(p);
+    public ApiResponse removeProduct(@RequestBody Product p) {
+        ApiResponse response=new ApiResponse();
+        response.setDescription("User not allowed to remove a product from this category.");
+        response.setResponseCode(13);
+        return response;
     }
 
     /**
