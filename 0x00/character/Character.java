e0893589903849e90e0ef46f09b49b84bffe56fa
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-25 01:51:26 +0400

    Task 1

--- 0x00/character/Character.java
+++ 0x00/character/Character.java
@@ -12,27 +12,17 @@ public abstract class Character {
     public Character(String name, CharacterType type, int intelligence, int strength, int endurance, int resilience, int dexterity) {
         this.name = name;
         this.type = type;
-        this.intelligence = intelligence; // "this.intelligence"
-        this.strength = strength;         // "this.strength"
-        this.endurance = endurance;       // "this.endurance"
-        this.resilience = resilience;     // "this.resilience"
-        this.dexterity = dexterity;       // "this.dexterity"
-
-        int checkInt = this.intelligence;
-        int checkStr = this.strength;
-        int checkEnd = this.endurance;
-        int checkRes = this.resilience;
-        int checkDex = this.dexterity;
+        this.intelligence = intelligence;
+        this.strength = strength;
+        this.endurance = endurance;
+        this.resilience = resilience;
+        this.dexterity = dexterity;
     }
 
     public abstract double getAttackDamage();
 
     @Override
     public String toString() {
-        return String.format(
-                "Character { name = %s, type = %s, intelligence = %d, strength = %d, endurance = %d, resilience = %d, dexterity = %d, attack damage = %.2f }",
-                name, type.name(), intelligence, strength, endurance, resilience, dexterity, getAttackDamage()
-        );
+        return String.format("Character { name = %s, type = %s, intelligence = %d, strength = %d, endurance = %d, resilience = %d, dexterity = %d, attack damage = %.2f }",name,type.name(),intelligence,strength,endurance,resilience,dexterity,getAttackDamage());
     }
 }
-
