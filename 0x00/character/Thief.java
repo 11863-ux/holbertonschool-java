fd5489fb89fd9afecd487bb8c6216d8c70332898
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-25 00:22:55 +0400

    Task 1

---
+++ 0x00/character/Thief.java
@@ -0,0 +1,13 @@
+package character;
+
+public class Thief extends Character {
+    public Thief(String name, int intelligence, int strength, int endurance, int resilience, int dexterity) {
+        super(name, CharacterType.THIEF, intelligence, strength, endurance, resilience, dexterity);
+        if (intelligence<=strength && intelligence<=dexterity) throw new IllegalArgumentException("Invalid attributes for THIEF");
+    }
+
+    @Override
+    double getAttackDamage() {
+        return (strength * 0.5) + (dexterity * 0.35) + (endurance * 0.1) + (intelligence * 0.05);
+    }
+}
