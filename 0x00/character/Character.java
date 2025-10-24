6cc53778354a892c1316a21a32e5ec584d198296
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-10-24 23:56:37 +0400

    Task 1

---
+++ 0x00/character/Character.java
@@ -0,0 +1,28 @@
+package character;
+
+public abstract class Character {
+    String name;
+    CharacterType type;
+    int intelligence;
+    int strength;
+    int endurance;
+    int resilience;
+    int dexterity;
+
+    public Character(String name, CharacterType type, int intelligence, int strength, int endurance, int resilience, int dexterity) {
+        this.name = name;
+        this.type = type;
+        this.intelligence = intelligence;
+        this.strength = strength;
+        this.endurance = endurance;
+        this.resilience = resilience;
+        this.dexterity = dexterity;
+    }
+
+    abstract double getAttackDamage();
+
+    @Override
+    public String toString() {
+        return String.format("Character { name = %s, type = %s, intelligence = %d, strength = %d, endurance = %d, resilience = %d, dexterity = %d, attack damage = %f }",name,type.name(),intelligence,strength,endurance,resilience,dexterity,getAttackDamage());
+    }
+}
