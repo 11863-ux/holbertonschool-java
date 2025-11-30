c2d1e3924056390c5275a6e24b038726b5f9e5d2
Mirhabil Sariyev <mirhabil@MIRHABIL-MacBook-Pro.local>
2025-11-30 09:32:17 +0400

    Task 1

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/Calculator.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/Calculator.java
@@ -3,23 +3,19 @@ package com.example.calculator.model;
 public class Calculator {
 
     public Double sum(Double number1, Double number2) {
-        // TODO
         // validation -> throw new NullPointerException("Number 1 and Number 2 are required.");
     }
 
     public Double sub(Double number1, Double number2) {
-        // TODO
         // validation -> throw new NullPointerException("Number 1 and Number 2 are required.");
     }
 
     public Double divide(Double number1, Double number2) {
-        // TODO
         // validation -> throw new NullPointerException("Number 1 and Number 2 are required.");
         // validation -> throw new ArithmeticException("Division by zero is not allowed.");
     }
 
     public Integer factorial(Integer factorial) {
-        // TODO
         // validation -> throw new NullPointerException("Number is required.");
     }
 
@@ -28,7 +24,6 @@ public class Calculator {
     // Integer = 5 -> Binary = 101
     // Integer = 20 -> Binary = 10100
     public Integer integerToBinary(Integer integer) {
-        // TODO
     }
 
     // Examples:
@@ -36,7 +31,6 @@ public class Calculator {
     // Integer = 55 -> Hexadecimal = "37"
     // Integer = 170 -> Hexadecimal = "AA"
     public String integerToHexadecimal(Integer integer) {
-        // TODO
     }
 
     // Examples
@@ -44,7 +38,6 @@ public class Calculator {
     // Date 2 = LocalDate.of(2020, 3, 29)
     // Total days = 14
     public int calculeDayBetweenDate(LocalDate date1, LocalDate date2) {
-        // TODO
     }
 
 }
